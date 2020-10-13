4c1d3ed137cba3196ba5724af3f94f0bc5e52201
Amanda Rolon <abrolon87@gmail.com>
2020-10-13 19:32:58 -0400

    adds addLog action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,8 +1,10 @@
 import { 
-  GET_LOGS, SET_LOADING, LOGS_ERROR
+  GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG
 } from "./types"
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
+  // is another way to do this, it is at the bottom of this file commented out
     try {
       setLoading()
 
@@ -20,8 +22,34 @@ export const getLogs = () => async dispatch => {
       })
     }
   }
-    
-  
+// add new log
+export const addLog = (log) => async dispatch => {
+  // is another way to do this, it is at the bottom of this file commented out
+    try {
+      setLoading()
+ 
+      const res = await fetch('/logs', { 
+        method: 'POST',
+        body: JSON.stringify(log),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      )
+      const data = await res.json()
+
+      dispatch({
+        type: ADD_LOG,
+        payload: data
+      })
+    } catch (err) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: err.response.data
+      })
+    }
+  }
+
 
 
 //sets loading to true
