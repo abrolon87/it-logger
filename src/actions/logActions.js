487a90004dd6f6f31b80b314aace64a0505806c5
Amanda Rolon <abrolon87@gmail.com>
2020-10-13 17:54:22 -0400

    builds logActions

---
+++ src/actions/logActions.js
@@ -0,0 +1,48 @@
+import { 
+  GET_LOGS, SET_LOADING, LOGS_ERROR
+} from "./types"
+
+export const getLogs = () => async dispatch => {
+    try {
+      setLoading()
+
+      const res = await fetch('/logs')
+      const data = await res.json()
+
+      dispatch({
+        type: GET_LOGS,
+        payload: data
+      })
+    } catch (err) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: err.response.data
+      })
+    }
+  }
+    
+  
+
+
+//sets loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  }
+}
+
+
+
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading()
+
+//     const res = await fetch('/logs')
+//     const data = await res.json()
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data
+//     })
+//   }
+// }
