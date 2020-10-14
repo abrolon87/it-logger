e8d10afd9a59a768aede6d96ead0be276c17e137
Amanda Rolon <abrolon87@gmail.com>
2020-10-13 22:56:31 -0400

    fixes typos

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -18,7 +18,7 @@ export const getLogs = () => async dispatch => {
     } catch (err) {
       dispatch({
         type: LOGS_ERROR,
-        payload: err.response.data
+        payload: err.response.statusText
       })
     }
   }
@@ -45,7 +45,7 @@ export const addLog = (log) => async dispatch => {
     } catch (err) {
       dispatch({
         type: LOGS_ERROR,
-        payload: err.response.data
+        payload: err.response.statusText
       })
     }
   }
@@ -66,7 +66,7 @@ export const addLog = (log) => async dispatch => {
       } catch (err) {
         dispatch({
           type: LOGS_ERROR,
-          payload: err.response.data
+          payload: err.response.statusText
         })
       }
     }
@@ -92,7 +92,7 @@ export const addLog = (log) => async dispatch => {
       } catch (err) {
         dispatch({
            type: LOGS_ERROR,
-           payload: err.response.data
+           payload: err.response.statusText
          })
       }
 }
@@ -112,7 +112,7 @@ export const searchLogs = (text) => async dispatch => {
     } catch (err) {
       dispatch({
         type: LOGS_ERROR,
-        payload: err.response.data
+        payload: err.response.statusText
       })
     }
   }
